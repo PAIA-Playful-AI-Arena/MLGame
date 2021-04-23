f0e6cbaf805c9c4ed65eb7b210ed1ec54988ef05
PTWA-Kylin <kylingithubdev@gmail.com>
2021-04-23 16:43:39 +0800

    test spawn

--- MLGame.py
+++ MLGame.py
@@ -1,6 +1,7 @@
 from mlgame.execution import execute
+import multiprocessing as mp
 
 if __name__ == "__main__":
-    # if not _winapi:
-    #     mp.set_start_method('spawn')
+
+    mp.set_start_method('spawn')
     execute()
