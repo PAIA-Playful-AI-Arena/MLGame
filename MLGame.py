c5bb23895302b699e5d582444594d58dbb6bc0e1
PTWA-Kylin <kylingithubdev@gmail.com>
2021-04-23 16:38:36 +0800

    use spawn

--- MLGame.py
+++ MLGame.py
@@ -1,4 +1,9 @@
+import _winapi
+
 from mlgame.execution import execute
+import multiprocessing as mp
 
 if __name__ == "__main__":
+    if not _winapi:
+        mp.set_start_method('spawn')
     execute()
