b13e8c968b2dcbc972d2582f439077cba82c56e5
hank <hankluo6@gmail.com>
2022-01-12 14:40:58 +0800

    Add flag to argument

--- mlgame/utils/parse_config.py
+++ mlgame/utils/parse_config.py
@@ -37,6 +37,11 @@ def parse_config(config_data):
                 else:
                     choices.append(choice)
             obj["choices"] = choices
+
+        if "flag" in param:
+            obj["name_or_flags"] = (f'-{param["flag"]}', f'--{param["name"]}')
+        else:
+            obj["name_or_flags"] = (f'--{param["name"]}',)
         result[param["name"]] = obj
     result["()"] = {
         "prog": config_data["game_name"],
