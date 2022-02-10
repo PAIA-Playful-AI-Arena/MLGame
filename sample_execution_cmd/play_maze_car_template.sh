65551474777f7b2703d1f1129f1e69f1438f10f9
Kylin_on_Mac <kylingithubdev@gmail.com>
2022-02-10 14:17:57 +0800

    update sample cmd

--- sample_execution_cmd/play_maze_car_template.sh
+++ sample_execution_cmd/play_maze_car_template.sh
@@ -1,2 +1,4 @@
-python MLGame.py -i ml_play_template.py -i ml_play_template.py -f 120 \
-Maze_Car --map 2 --game_type MAZE --user_num 2 --time_to_play 450 --sensor_num 5
+python MLGame.py \
+-i ml_play_template.py -i ml_play_template.py -i ml_play_template.py \
+-i ml_play_template.py -i ml_play_template.py -i ml_play_template.py -f 120 \
+Maze_Car --map 1 --game_type MAZE --user_num 6 --time_to_play 450 --sensor_num 5 --sound off
