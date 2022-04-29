79fc8b044f5244a7b25216a2770e4ff61b41c946
Kylin_on_Mac <kylingithubdev@gmail.com>
2022-04-29 14:13:05 +0800

    update

--- sample_execution_cmd/play_easy_game.sh
+++ sample_execution_cmd/play_easy_game.sh
@@ -1,2 +1,2 @@
-python -m mlgame -i ./games/easy_game/ml/ml_play_template.py -f 60 \
+python -m mlgame -i ./games/easy_game/ai_clients/client_template.py -f 60 \
 ./games/easy_game  --score 10 --color FF9800 --time_to_play 600 --total_point 50
