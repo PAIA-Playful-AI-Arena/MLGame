55cc7b488ebac0d55cbd9c948994d6cef154c209
Kylin_on_Mac <kylingithubdev@gmail.com>
2022-05-04 11:16:11 +0800

    update readme and add ball_served data in  scene_info

--- sample_execution_cmd/play_pingpong_template.sh
+++ sample_execution_cmd/play_pingpong_template.sh
@@ -1,2 +1,2 @@
-python MLGame.py -i ml_play_template_1P.py -i ml_play_template_2P.py -f 120 \
+python MLGame.py -i ml_play_template_1P.py -i ml_play_template_2P.py -f 20 \
 pingpong --difficulty HARD --game_over_score 5
