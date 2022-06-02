2dd52a188e489f9b4576ddde29b9b2184607a8d6
Kylin_on_Mac <kylingithubdev@gmail.com>
2022-06-02 11:12:21 +0800

    update temp

--- sample_execution_cmd/play_arkanoid_template.sh
+++ sample_execution_cmd/play_arkanoid_template.sh
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 python -m mlgame -i ./PAIA_Game_Code/arkanoid/rule/ml_play.py -f 120 ./games/arkanoid --difficulty NORMAL --level 3
+=======
+python MLGame.py -i ml_play_template.py -f 120 arkanoid --difficulty NORMAL --level 5
+>>>>>>> develop
--- sample_execution_cmd/play_pingpong_template.sh
+++ sample_execution_cmd/play_pingpong_template.sh
@@ -1,2 +1,7 @@
+<<<<<<< HEAD
 python -m mlgame  -f 20 -i ./games/pingpong/ml/ml_play_template_1P.py -i ./games/pingpong/ml/ml_play_template_2P.py \
 ./games/pingpong/ --difficulty HARD --game_over_score 5
+=======
+python MLGame.py -i ml_play_template_1P.py -i ml_play_template_2P.py -f 20 \
+pingpong --difficulty HARD --game_over_score 5
+>>>>>>> develop
