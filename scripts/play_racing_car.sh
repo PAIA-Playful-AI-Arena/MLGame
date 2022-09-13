9a51c382df0cd2578bf9f012a3cfd0013e3457e7
Kylin_on_Mac <kylingithubdev@gmail.com>
2022-09-13 16:04:16 +0800

    fix bug: ai client will be broken down when drag game window

--- scripts/play_racing_car.sh
+++ scripts/play_racing_car.sh
@@ -1,3 +1,2 @@
 python -m mlgame -i ./games/racing_car/ml/ml_play_template.py -f 120 \
---ws "wss://demo.piesocket.com/v3/paia?api_key=VCXCEuvhGcBDP7XhiJJUDvR1e1D3eiVjgZ9VRiaV" \
-./games/RacingCar/
+./games/racing_car/ --racetrack_length 1000
