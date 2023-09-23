6ac416590b90a75c0d4c45242f764cbc0c3143d8
Kylin_on_Mac <kylingithubdev@gmail.com>
2023-09-23 11:38:17 +0800

    doc:add git push script

--- scripts/git_push.sh
+++ scripts/git_push.sh
@@ -4,3 +4,4 @@ latest_tag=$(git describe --tags $(git rev-list --tags --max-count=1))
 git push github master develop $latest_tag
 git push gitlab master develop $latest_tag
 git push paia master develop $latest_tag
+git checkout develop
